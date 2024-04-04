365041f4f07eeeec0afa55c49d5fc9988ac3fb28
yyp <3304084960@qq.com>
2024-04-04 22:57:31 +0800

    开发邮件发送功能

---
+++ src/test/java/com/nowcoder/community/MailTests.java
@@ -0,0 +1,46 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.util.MailClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+/**
+ * @Author: yyp
+ * @Date: 2024/4/4 - 04 - 04 - 22:34
+ * @Description: com.nowcoder.community
+ * @version: 1.0
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MailTests {
+
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Test
+    public void testTextMail() {
+        mailClient.sendMail("3304084960@qq.com", "TEST", "Welcome");
+    }
+
+    @Test
+    public void testHtmlMail() {
+        Context context = new Context();
+        context.setVariable("username", "sunday");
+
+        String content = templateEngine.process("/mail/demo", context);
+        System.out.println(content);
+
+        mailClient.sendMail("3304084960@qq.com", "HTML", content);
+    }
+
+}
