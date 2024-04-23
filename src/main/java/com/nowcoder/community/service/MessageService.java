c78c546c11f35f26ad725cb3509b1596a1c01012
yyp <3304084960@qq.com>
2024-04-23 11:52:47 +0800

    私信列表

---
+++ src/main/java/com/nowcoder/community/service/MessageService.java
@@ -0,0 +1,43 @@
+package com.nowcoder.community.service;
+
+import com.nowcoder.community.dao.MessageMapper;
+import com.nowcoder.community.entity.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @Author: yyp
+ * @Date: 2024/4/23 - 04 - 23 - 10:16
+ * @Description: com.nowcoder.community.service
+ * @version: 1.0
+ */
+
+@Service
+public class MessageService {
+
+    @Autowired
+    private MessageMapper messageMapper;
+
+    public List<Message> findConversations(int userId, int offset, int limit) {
+        return messageMapper.selectConversations(userId, offset, limit);
+    }
+
+    public int findConversationCount(int userId) {
+        return messageMapper.selectConversationCount(userId);
+    }
+
+    public List<Message> findLetters(String conversationId, int offset, int limit) {
+        return messageMapper.selectLetters(conversationId, offset, limit);
+    }
+
+    public int findLetterCount(String conversationId) {
+        return messageMapper.selectLetterCount(conversationId);
+    }
+
+    public int findLetterUnreadCount(int userId, String conversationId) {
+        return messageMapper.selectLetterUnreadCount(userId, conversationId);
+    }
+
+}
