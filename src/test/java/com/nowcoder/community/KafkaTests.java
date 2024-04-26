b42ca2a636ecaf61bc50e087b6aa48c2abaa2b68
yyp <3304084960@qq.com>
2024-04-26 13:08:46 +0800

    Spring整合kafka

---
+++ src/test/java/com/nowcoder/community/KafkaTests.java
@@ -0,0 +1,62 @@
+package com.nowcoder.community;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @Author: yyp
+ * @Date: 2024/4/26 - 04 - 26 - 12:47
+ * @Description: com.nowcoder.community
+ * @version: 1.0
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class KafkaTests {
+
+    @Autowired
+    private KafkaProducer kafkaProducer;
+
+    @Test
+    public void testKafka() {
+        kafkaProducer.sendMessage("test", "你好");
+        kafkaProducer.sendMessage("test", "在线吗?");
+
+        try {
+            Thread.sleep(1000 * 10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+@Component
+class KafkaProducer {
+
+    @Autowired
+    private KafkaTemplate kafkaTemplate;
+
+    public void sendMessage(String topic, String content) {
+        kafkaTemplate.send(topic, content);
+    }
+
+}
+
+@Component
+class KafkaConsumer {
+
+    @KafkaListener(topics = {"test"}, groupId = "${spring.kafka.consumer.group-id}")
+    public void handleMessage(ConsumerRecord record) {
+        System.out.println(record.value());
+    }
+
+
+}
